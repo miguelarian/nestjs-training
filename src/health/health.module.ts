4e9dffea362ce1ba1c9c5d301a2931431a5b03e3
Miguel Vela Romera <miguelvr88@gmail.com>
2025-11-15 14:33:38 +0100

    import config module

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthService } from './health.service';
 import { HealthController } from './health.controller';
+import { ConfigModule } from 'src/config/config.module';
 
 @Module({
+    imports: [ConfigModule],
     providers: [HealthService],
     exports: [HealthService],
     controllers: [HealthController],
