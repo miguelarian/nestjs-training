72cd562443162ea6c572e33ab3882b8dc186f6fa
Miguel Vela Romera <miguelvr88@gmail.com>
2025-11-15 14:50:50 +0100

    add episodes and config modules to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { EpisodesController } from './episodes/episodes.controller';
+import { EpisodesModule } from './episodes/episodes.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [HealthModule],
-  controllers: [AppController],
+  imports: [HealthModule, EpisodesModule, ConfigModule],
+  controllers: [AppController, EpisodesController],
   providers: [AppService],
 })
 export class AppModule {}
