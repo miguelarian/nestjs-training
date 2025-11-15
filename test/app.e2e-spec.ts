5b312ae793777911c0d9605c6f7ccdb579727662
Miguel Vela Romera <miguelvr88@gmail.com>
2025-11-15 11:44:07 +0100

    rename test names to be more explanatory

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,7 +4,7 @@ import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Root module path /', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -16,10 +16,9 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('GET root path / should return 200', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
   });
 });
