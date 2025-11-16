34478ed4b2012b8402a258e645bc78ab2914a93c
Miguel Vela Romera <miguelvr88@gmail.com>
2025-11-16 19:23:39 +0100

    fix app module dependency injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,12 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { HealthModule } from "./health/health.module";
-import { EpisodesController } from "./episodes/episodes.controller";
 import { EpisodesModule } from "./episodes/episodes.module";
 import { ConfigModule } from "./config/config.module";
-import { EpisodesService } from "./episodes/episodes.service";
-import { ConfigService } from "./config/config.service";
 
 @Module({
   imports: [HealthModule, EpisodesModule, ConfigModule],
-  controllers: [AppController, EpisodesController],
-  providers: [AppService, EpisodesService, ConfigService],
+  controllers: [AppController], // EpisodesController provided by EpisodesModule
+  providers: [AppService], // EpisodesService & ConfigService provided by their modules
 })
 export class AppModule {}
